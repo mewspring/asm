88b5fe1d8eebc7e79c81f838f046fbfc54eaaa41
mewmew <rnd0x00@gmail.com>
2014-04-04 01:25:15 +0200

    lexer: Add package lexer which implements tokenization of assembly source text.

---
+++ lexer/lexer.go
@@ -0,0 +1,223 @@
+// The implementation of this package is heavily inspired by Rob Pike's amazing
+// talk titled "Lexical Scanning in Go" [1].
+//
+// The assembly language specification have been based upon the Go Programming
+// Language Specification [2] and some comments are therefore governed by a
+// BSD-style license [3]. Any original content is hereby released into the
+// public domain [4].
+//
+// [1]: https://www.youtube.com/watch?v=HxaD_trXwRE
+// [2]: http://golang.org/ref/spec
+// [3]: http://golang.org/LICENSE
+// [4]: https://creativecommons.org/publicdomain/zero/1.0/
+
+// Package lexer implements tokenization of assembly source text.
+package lexer
+
+import (
+	"fmt"
+	"log"
+	"strings"
+	"unicode"
+	"unicode/utf8"
+
+	"github.com/mewmew/asm/token"
+)
+
+const (
+	// eof is the rune returned by next when no more input is available.
+	eof = -1
+)
+
+// A lexer lexes the input into tokens.
+type lexer struct {
+	// The input text.
+	input string
+	// The start position of the current token.
+	start int
+	// The current position in the input.
+	pos int
+	// The width in byte of the last rune read with next.
+	width int
+	// A slice of scanned tokens.
+	tokens []token.Token
+}
+
+// Parse lexes the provided input and returns a slice of scanned tokens.
+func Parse(input string) (tokens []token.Token) {
+	l := &lexer{
+		input:  input,
+		tokens: make([]token.Token, 0),
+	}
+
+	// Tokenize the input.
+	l.run()
+
+	return l.tokens
+}
+
+// run lexes the input by repeatedly executing the active state function until
+// it returns a nil state.
+func (l *lexer) run() {
+	// lexLine is the initial state function of the lexer.
+	for state := lexLine; state != nil; {
+		state = state(l)
+	}
+}
+
+// errorf emits an error token and terminates the scan by returning a nil state
+// function.
+func (l *lexer) errorf(format string, args ...interface{}) stateFn {
+	tok := token.Token{
+		Typ: token.Error,
+		Val: fmt.Sprintf(format, args...),
+	}
+	l.tokens = append(l.tokens, tok)
+	return nil
+}
+
+// emitEOF emits an EOF token and terminates the scan by returning a nil state
+// function.
+func (l *lexer) emitEOF() stateFn {
+	if l.pos < len(l.input) {
+		log.Fatalf("lexer.emitEOF: unexpected eof; pos %d < len(input) %d.\n", l.pos, len(l.input))
+	}
+	if l.start != l.pos {
+		log.Fatalf("lexer.emitEOF: invalid eof; pending input %q not handled.\n", l.input[l.start:])
+	}
+	l.emit(token.EOF)
+	return nil
+}
+
+// emit emits a token of the provided token type and advances the token start
+// position.
+func (l *lexer) emit(typ token.Type) {
+	tok := token.Token{
+		Typ: typ,
+		Val: l.input[l.start:l.pos],
+	}
+	l.tokens = append(l.tokens, tok)
+	l.start = l.pos
+}
+
+// next consumes the next rune of the input.
+func (l *lexer) next() (r rune) {
+	if l.pos >= len(l.input) {
+		l.width = 0
+		return eof
+	}
+	r, l.width = utf8.DecodeRuneInString(l.input[l.pos:])
+	l.pos += l.width
+	return r
+}
+
+// peek returns but does not consume the next rune of the input.
+func (l *lexer) peek() (r rune) {
+	r = l.next()
+	if r == eof {
+		return eof
+	}
+	l.backup()
+	return r
+}
+
+// backup backs up one rune in the input. It can only be called once per call to
+// next.
+func (l *lexer) backup() {
+	if l.width == 0 {
+		// TODO(u): Handle eof elsewhere so we never hit this case.
+		log.Fatalln("lexer.backup: invalid width; no matching call to next.")
+	}
+	l.pos -= l.width
+	l.width = 0
+}
+
+// isLetter returns true if r is a Unicode letter or an underscore, and false
+// otherwise.
+func isLetter(r rune) bool {
+	return unicode.IsLetter(r) || r == '_'
+}
+
+// isLetterOrDigit returns true if r is a Unicode letter, an underscore or a
+// digit, and false otherwise.
+func isLetterOrDigit(r rune) bool {
+	return isLetter(r) || unicode.IsDigit(r)
+}
+
+// isSpace returns true if r is a whitespace character, and false otherwise.
+func isSpace(r rune) bool {
+	// whitespace characters, except newline.
+	const whitespace = " \t"
+
+	return strings.IndexRune(whitespace, r) != -1
+}
+
+// accept consumes the next rune if it's from the valid set. It returns true if
+// a rune was consumed and false otherwise.
+func (l *lexer) accept(valid string) bool {
+	r := l.next()
+	if r == eof {
+		return false
+	}
+	if strings.IndexRune(valid, r) == -1 {
+		l.backup()
+		return false
+	}
+	return true
+}
+
+// acceptFunc consumes the next rune if it's valid. It returns true if a rune
+// was consumed and false otherwise.
+func (l *lexer) acceptFunc(isValid func(rune) bool) bool {
+	r := l.next()
+	if r == eof {
+		return false
+	}
+	if !isValid(r) {
+		l.backup()
+		return false
+	}
+	return true
+}
+
+// acceptRun consumes a run of runes from the valid set. It returns true if a
+// rune was consumed and false otherwise.
+func (l *lexer) acceptRun(valid string) bool {
+	consumed := false
+	for l.accept(valid) {
+		consumed = true
+	}
+	return consumed
+}
+
+// acceptRunFunc consumes a run of valid runes. It returns true if a rune was
+// consumed and false otherwise.
+func (l *lexer) acceptRunFunc(isValid func(rune) bool) bool {
+	consumed := false
+	for l.acceptFunc(isValid) {
+		consumed = true
+	}
+	return consumed
+}
+
+// acceptIdent consumes an identifier. It returns true if a rune was consumed
+// and false otherwise.
+func (l *lexer) acceptIdent() bool {
+	if !l.acceptFunc(isLetter) {
+		return false
+	}
+	l.acceptRunFunc(isLetterOrDigit)
+	return true
+}
+
+// ignore ignores any pending input read since the last token.
+func (l *lexer) ignore() {
+	l.start = l.pos
+}
+
+// ignoreRun ignores a run of valid runes.
+func (l *lexer) ignoreRun(valid string) {
+	if l.acceptRun(valid) {
+		l.ignore()
+	}
+}
---
+++ lexer/state.go
@@ -0,0 +1,267 @@
+package lexer
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/mewmew/asm/token"
+)
+
+// A stateFn represents the state of the scanner as a function that returns a
+// state function.
+type stateFn func(l *lexer) stateFn
+
+// lexLine lexes a line. It is the initial state function of the lexer.
+func lexLine(l *lexer) stateFn {
+	for {
+		r := l.next()
+		switch {
+		case r == eof:
+			return l.emitEOF()
+		case isSpace(r):
+			// Ignore whitespace.
+			l.ignore()
+		case isLetter(r):
+			l.backup()
+			// Lex label, operator, register or directive identifier.
+			return lexIdent
+		case r == '.':
+			// Lex local label identifier.
+			// TODO(u): Add support for floating-point literals.
+			return lexIdent
+		case r == ':':
+			// Lex label declaration colon.
+			return lexColon
+		case r == '+' || r == '-' || r >= '0' && r <= '9':
+			// Lex integer literal.
+			l.backup()
+			return lexIntLit
+		case r == '\'':
+			// Lex character literal.
+			return lexCharLit
+		case r == '"':
+			// Lex string literal.
+			return lexStringLit
+		case r == '`':
+			// Lex raw string literal.
+			return lexRawStringLit
+		case r == '$':
+			// Lex register identifier.
+			return lexIdent
+		case r == ',':
+			// Lex operand separation comma
+			return lexComma
+		case r == ';':
+			// Lex line comment.
+			return lexLineComment
+		case r == '\n':
+			// Lex newline.
+			return lexNewline
+		default:
+			return l.errorf("lexer.lexLine: unexpected rune '%c' at beginning of token", r)
+		}
+	}
+}
+
+// lexIdent lexes an identifier. A '.' or a '$' may have been consumed already.
+func lexIdent(l *lexer) stateFn {
+	if !l.acceptFunc(isLetter) {
+		return l.errorf("lexer.lexIdent: expected Unicode letter or underscore, got '%c'", l.next())
+	}
+	l.acceptRunFunc(isLetterOrDigit)
+	l.emit(token.Ident)
+	return lexLine
+}
+
+// lexColon lexes a label declaration colon. A ':' has already been consumed.
+func lexColon(l *lexer) stateFn {
+	l.emit(token.Colon)
+	return lexLine
+}
+
+// lexIntLit lexes an integer literal. No characters have been consumed already.
+func lexIntLit(l *lexer) stateFn {
+	// Optional sign.
+	l.accept("+-")
+
+	// Decimal digits.
+	digits := "0123456789"
+	isZero := false
+	r := l.next()
+	switch {
+	case r == eof:
+		return l.emitEOF()
+	case r == '0':
+		switch {
+		case l.accept("xX"):
+			// Hexadecimal digits.
+			digits = "0123456789abcdefABCDEF"
+		case l.accept("b"):
+			// Binary digits.
+			digits = "01"
+		default:
+			// Octal digits.
+			digits = "01234567"
+			isZero = true
+		}
+	case r < '1' || r > '9':
+		return l.errorf("lexer.lexIntLit: expected decimal digit, got '%c", r)
+	}
+	if !l.acceptRun(digits) && !isZero {
+		return l.errorf("lexer.lexIntLit: missing digits in integer literal")
+	}
+	l.emit(token.Int)
+
+	return lexLine
+}
+
+// lexCharLit lexes a character literal. A single quote has already been
+// consumed.
+func lexCharLit(l *lexer) stateFn {
+	if l.accept(`\`) {
+		// Consume backslash escape sequence.
+		err := consumeEscape(l, '\'')
+		if err != nil {
+			return l.errorf("lexer.lexCharLit: %v", err)
+		}
+	} else {
+		// Consume single quoted character. Any character may appear except single
+		// quote and newline.
+		r := l.next()
+		switch r {
+		case eof:
+			return l.errorf("lexer.lexCharLit: unexpected eof in single quoted character literal")
+		case '\'':
+			return l.errorf("lexer.lexCharLit: unexpected ' in character literal")
+		case '\n':
+			return l.errorf("lexer.lexCharLit: unexpected newline in character literal")
+		}
+	}
+	if !l.accept("'") {
+		return l.errorf("lexer.lexCharLit: missing ' in character literal")
+	}
+	l.emit(token.Char)
+	return lexLine
+}
+
+// lexStringLit lexes a string literal. A '"' has already been consumed.
+func lexStringLit(l *lexer) stateFn {
+	for {
+		r := l.next()
+		switch r {
+		case eof:
+			return l.errorf("lexer.lexStringLiteral: unexpected eof in string literal")
+		case '\n':
+			return l.errorf("lexer.lexStringLiteral: unexpected newline in string literal")
+		case '\\':
+			// Consume backslash escape sequence.
+			err := consumeEscape(l, '"')
+			if err != nil {
+				return l.errorf("lexer.lexStringLit: %v", err)
+			}
+		case '"':
+			l.emit(token.String)
+			return lexLine
+		}
+	}
+}
+
+// lexRawStringLit lexes a raw string literal. A '`' has already been consumed.
+func lexRawStringLit(l *lexer) stateFn {
+	for {
+		r := l.next()
+		switch r {
+		case eof:
+			return l.errorf("lexer.lexRawStringLiteral: unexpected eof in raw string literal")
+		case '`':
+			l.emit(token.String)
+			return lexLine
+		}
+	}
+}
+
+// consumeEscape consumes an escape sequence. A backslash has already been
+// consumed. A valid single-character escape sequence is specified by valid.
+// Single quotes are only valid within character literals and double quotes are
+// only valid within string literals.
+func consumeEscape(l *lexer, valid rune) (err error) {
+	// Several backslash escapes allow arbitrary values to be encoded as ASCII
+	// text. There are two ways to represent the integer value as a numeric
+	// constant: \x followed by exactly two hexadecimal digits, and a plain
+	// backslash \ followed by exactly three octal digits. Although these
+	// representations all result in an integer, they have different valid
+	// ranges. Octal escapes must represent a value between 0 and 255 inclusive.
+	//
+	// After a backslash, certain single-character escapes represent special
+	// values:
+	//
+	//    \a   U+0007 alert or bell
+	//    \b   U+0008 backspace
+	//    \f   U+000C form feed
+	//    \n   U+000A line feed or newline
+	//    \r   U+000D carriage return
+	//    \t   U+0009 horizontal tab
+	//    \v   U+000b vertical tab
+	//    \\   U+005c backslash
+	//    \'   U+0027 single quote  (valid escape only within character literals)
+	//    \"   U+0022 double quote  (valid escape only within string literals)
+	//
+	// All other sequences starting with a backslash are illegal inside character
+	// literals.
+	r := l.next()
+	switch r {
+	case eof:
+		return errors.New("lexer.consumeEscape: unexpected eof after backslash escape character")
+	case '0', '1', '2', '3':
+		// Octal escape sequence.
+		digits := "01234567"
+		if !l.accept(digits) || !l.accept(digits) {
+			return fmt.Errorf("lexer.consumeEscape: non-octal character in escape sequence: %c", l.next())
+		}
+	case 'x':
+		// Hexadecimal escape sequence.
+		digits := "0123456789abcdefABCDEF"
+		if !l.accept(digits) || !l.accept(digits) {
+			return fmt.Errorf("lexer.consumeEscape: non-hex character in escape sequence: %c", l.next())
+		}
+	case 'a', 'b', 'f', 'n', 'r', 't', 'v', '\\':
+		// Consume single-character escape.
+	default:
+		// Single quote escapes are only valid within character literals and
+		// double quote escapes are only valid within string literals.
+		if r != valid {
+			return fmt.Errorf("lexer.consumeEscape: unknown escape sequence: %c", r)
+		}
+	}
+
+	return nil
+}
+
+// lexComma lexes an operand separation comma. A ',' has already been consumed.
+func lexComma(l *lexer) stateFn {
+	l.emit(token.Comma)
+	return lexLine
+}
+
+// lexLineComment lexes a line comment. A ';' has already been consumed.
+func lexLineComment(l *lexer) stateFn {
+	for {
+		r := l.next()
+		switch r {
+		case eof:
+			l.emit(token.LineComment)
+			l.emit(token.EOF)
+			return nil
+		case '\n':
+			l.backup()
+			l.emit(token.LineComment)
+			return lexLine
+		}
+	}
+}
+
+// lexNewline lexes a newline. A '\n' has already been consumed.
+func lexNewline(l *lexer) stateFn {
+	l.emit(token.Newline)
+	return lexLine
+}
