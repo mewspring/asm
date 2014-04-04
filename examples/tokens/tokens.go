df0cd66f9df16c764548abbaeeb24c374309423c
mewmew <rnd0x00@gmail.com>
2014-04-04 02:13:41 +0200

    examples/tokens: Add tokens example command which demonstrates how to tokenize input files.

---
+++ examples/tokens/tokens.go
@@ -0,0 +1,39 @@
+// tokens demonstrates how to tokenize input files.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/mewmew/asm/lexer"
+)
+
+func main() {
+	flag.Parse()
+	for _, filePath := range flag.Args() {
+		err := tokens(filePath)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+}
+
+// tokens lexes the provided file and prints each token to standard output.
+func tokens(filePath string) (err error) {
+	// Read input from file.
+	buf, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+	input := string(buf)
+
+	// Tokenize the input.
+	tokens := lexer.Parse(input)
+	for _, tok := range tokens {
+		fmt.Println("tok:", tok)
+	}
+
+	return nil
+}
